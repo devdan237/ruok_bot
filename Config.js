c1a9b11ae7e9d00abd3719156e5968373c1c65d0
devdan237 <gs3717896@gmail.com>
2025-02-22 20:56:09 +0100

    Config.js

--- Config.js
+++ Config.js
@@ -8,4 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Y28j0awL#NAWK6UT9DHcyCPXJqmHScJ-GblA1tbaR0RcNQl7l2qE",
 MONGODB: process.env.MONGODB || "mongodb+srv://Maher:Zubair@sigma-male.ggwx4gc.mongodb.net/?retryWrites=true&w=majority",
   MODE :  process.env.MODE || "", // PUT PUBLIC OR PRIVATE
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/x5v8x1.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi 👋, I am RUOK V1.0 and i am alive 🥺. Thanks for supporting my developer 💦.",
 };
