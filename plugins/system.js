25e9807a8b4f3e04859b68a9f5f26fdf9d1f6c49
devdan237 <gs3717896@gmail.com>
2025-02-24 09:39:05 +0100

    system.js

---
+++ plugins/system.js
@@ -0,0 +1,30 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+cmd({
+    pattern: "system",
+    react: "🧾",
+    alias: ["uptime","status","runtime"],
+    desc: "cheack uptime",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `*RUOK V1.0 UPTIME LIST*
+
+*UPTIME:*  ${runtime(process.uptime())}
+
+*RAM USAGE:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+
+*HOSTNAME:* ${os.hostname()}
+
+*OWNER:* *DEVDAN237*
+`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
