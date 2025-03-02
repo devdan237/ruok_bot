93c59c59a8a48367bbfb9a093f0e26b11ee34b64
DevDan237 <gs3717896@gmail.com>
2025-03-02 01:59:49 +0100

    Create fancy.js

---
+++ plugins/fancy.js
@@ -0,0 +1,24 @@
+const { ruok } = require("../framework/ruok");
+const fancy = require("../commandes/style");
+
+ruok({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+    const { arg, repondre, prefixe } = commandeOptions;
+    const id = arg[0]?.match(/\d+/)?.join('');
+    const text = arg.slice(1).join(" ");
+
+    try {
+        if (id === undefined || text === undefined) {
+            return await repondre(`\nExemple : ${prefixe}fancy 10 ruok v1.0\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Ruok V1.0', fancy));
+        }
+
+        const selectedStyle = fancy[parseInt(id) - 1];
+        if (selectedStyle) {
+            return await repondre(fancy.apply(selectedStyle, text));
+        } else {
+            return await repondre('_Style introuvable :(_');
+        }
+    } catch (error) {
+        console.error(error);
+        return await repondre('_Une erreur s\'est produite :(_');
+    }
+});
